8f415a2e2c94d10e2fa23a0ed783b56c35d082ef
Doffensmirthz <drdoffensmirthz@gmail.com>
2025-12-14 22:46:38 +0000

    Add files via upload

---
+++ trigger.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# lanzar player
+player /home/ic/installations/player-stage/stage-2.1.1/worlds/everything.cfg & 
+PLAYER_PID=$!
+
+sleep 10
+
+/home/ic/code/clientev3
+
+kill $PLAYER_PID
+wait $PLAYER_PID
